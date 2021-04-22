6faa804de065bac47fae918a54a476a5ef215ddb
d116626 <d116626@gmail.com>
2021-04-22 15:10:36 -0300

    all

--- scripts/orcamento_sp.py
+++ scripts/orcamento_sp.py
@@ -7,6 +7,7 @@ import pandas as pd
 import numpy as np
 from scripts.manipulate import normalize_cols, padronize_str
 import re
+import math
 
 
 def generic_group(df, group_cols, numeric_cols, sort_cols):
@@ -69,6 +70,40 @@ def get_despesas(files):
     return df_despesa, df_cols
 
 
+def receita_origem(df_receita):
+    ### Origem da receita
+    df_receita["id_origem"] = df_receita["id_receita"].apply(lambda x: x[:2])
+    origem_receita = {
+        "11": "Receita Tributária",
+        "12": "Receita de Contribuições",
+        "13": "Receita Patrimonial",
+        "14": "Receita Agropecuária",
+        "15": "Receita Industrial",
+        "16": "Receita de Serviços",
+        "17": "Transferências Correntes",
+        "19": "Outras Receitas Correntes",
+        "21": "Operações de Crédito",
+        "22": "Alienação de Bens",
+        "23": "Amortização de Empréstimos",
+        "24": "Traferência de Capital",
+        "25": "Outras Receitas de Capital do Estado",
+        "29": "Demais Receitas de Capital",
+        "72": "Receitas de Contribuições - Intra-Orçamentárias",
+        "74": "Receita Intra-Orçamentárias com Receitas de Agropecuaria",
+        "76": "Receitas Intra-Orçamentárias com Receitas de Serviços",
+        "77": "Receita de Transferencia Intra-Orçamentárias COVID",
+        "79": "Receitas Intra-Orçamentárias com Outras Receitas Correntes",
+        "82": "Alienação de Bens",
+        "85": "Outras Receitas de Capital",
+        "89": "Outras Receitas de Capital",
+    }
+    df_receita["origem"] = (
+        df_receita["id_origem"] + " - " + df_receita["id_origem"].map(origem_receita)
+    )
+
+    return df_receita
+
+
 def get_receita(files):
     ## CHECA SE ARQUIVOS SAO CONSISTENTES ENTRE OS ANOS
     df_cols = pd.DataFrame()
@@ -108,4 +143,10 @@ def get_receita(files):
 
     df_receita = df_receita.drop(["unnamed:_6"], 1)
 
+    df_receita["id_receita"] = df_receita["receita"].apply(
+        lambda x: x.split("-")[0].strip()
+    )
+
+    ### Origem da receita
+    df_receita = receita_origem(df_receita)
     return df_receita, df_cols
